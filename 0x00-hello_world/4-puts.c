9d7b83ac78c31aa0f22d30f88fda6eeab4ab3248
Thato <thatomsimanga@gmail.com>
2023-09-07 13:23:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *  main - print a line with puts function
+ *  Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
