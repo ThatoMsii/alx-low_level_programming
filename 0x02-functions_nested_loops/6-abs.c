6423b099026fd92a7f265d581039a0f12f7392c1
Thato <thatomsimanga@gmail.com>
2023-09-12 16:31:54 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_abs - Determines the absolute value
+ *of an integer.
+ *
+ *@i: input number as an integer.
+ *Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return ('j');
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
