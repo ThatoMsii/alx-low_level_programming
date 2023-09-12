2ef4adb4212d21caafe982223da00cc96802f371
Thato <thatomsimanga@gmail.com>
2023-09-12 13:18:21 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	write(1, &letter, 1);
+	letter++;
+	}
+}
