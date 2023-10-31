b44c4fdeb1e3673bbd93261b4be17e13a3dfd75f
Thato <thatomsimanga@gmail.com>
2023-10-31 06:23:41 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sstdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fontl.h>
+#include <fcntl.h>
 #include <unistd.h>
 
  int _putchar(char c);
